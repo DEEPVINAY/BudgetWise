1511bfbf0c4117ba599dafd6fdb45e10e9f7ca33
Deep Vinay Parisarla <deepvinayparisarla@Deeps-MacBook-Pro.local>
2026-03-10 14:39:05 +0530

    added devops pipeline files

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  "projectId": "studio-7989988439-f815c",
+  "appId": "1:611589163887:web:b5263ac78f9f45fe77f69a",
+  "apiKey": "AIzaSyBqah_pfsgwrkiBPVbLWzIDoLzqh9w5DiY",
+  "authDomain": "studio-7989988439-f815c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "611589163887"
 };
+
